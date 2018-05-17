e6568f5de471aa8dbe21bccee73e10488d1f0e66
Jack-rainbow <987709173@qq.com>
2018-05-17 14:51:57 +0800

    051701

---
+++ docs/.vuepress/components/OtherComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="hello">{{ message }}</div>
+        <el-button>默认按钮</el-button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'OtherComponent',
+    data(){
+        return {
+            message: "search"
+        }
+    }
+}
+</script>
+<style lang="stylus">
+    div
+        .hello
+            color : red
+     
+</style>
---
+++ docs/blog/README.md
@@ -0,0 +1,7 @@
+#### 主页
+:m:
+
+
+
+
+<OtherComponent/>
